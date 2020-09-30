635c387290430454ad982bd308f167b78c6dc9ea
benvaro <benvaro91@gmail.com>
2020-09-30 18:51:00 +0300

    events

---
+++ 29_Events/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_Events
+{
+    class Program
+    {
+        delegate void MyDelegate();
+
+        class MyEvent
+        {
+            public event MyDelegate userEvent; // захищений делегат, дозволяємо лише += та -=
+            // не допускається = (заміна) = null (очищення)
+             
+            public void OnSomethingHapping() //викликати, ініціювати виклик події
+            {
+                // if(userEvemt != null)   userEvent(); 
+                userEvent?.Invoke();
+            }
+        }
+
+        class User
+        {
+            public string Name { get; set; }
+            public string LastName { get; set; }
+
+            public void UserInfoHandler() // EventHandler - обробник події
+            {
+                Console.WriteLine("Event was initiated");
+                Console.WriteLine($"By {Name} {LastName}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MyEvent ev = new MyEvent();
+            
+            User u1 = new User { Name = "Andriy", LastName = "Poberezhnyi" };
+            User u2 = new User { Name = "Olia", LastName = "Kulyk" };
+
+            ev.userEvent += u1.UserInfoHandler;
+            ev.userEvent += u2.UserInfoHandler;
+
+            ev.OnSomethingHapping(); // 
+       //     ev.userEvent = null; // помилка, бо оголосили як event
+            ev.OnSomethingHapping();
+        }
+    }
+}
---
+++ 30_EventHandler/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_EventHandler
+{
+    class Program
+    {
+        delegate void MyDelegate();
+
+        class Eventer
+        {
+            public event MyDelegate SomethingHappen;
+
+            public void DoSmth(int number)
+            {
+                Console.WriteLine("Number: {0}", number);
+                if (number > 5 && number < 15)
+                    OnSomethingHappen();
+            }
+
+            private void OnSomethingHappen()
+            {
+                SomethingHappen?.Invoke();
+            }
+        }
+
+        class One
+        {
+            public static void Handler()
+            {
+                Console.WriteLine("It is a static Handler from class One");
+            }
+        }
+
+        class Two
+        {
+            public  void Handler()
+            {
+                Console.WriteLine("It is a  Handler from class Two");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Eventer ev = new Eventer();
+            Two two = new Two();
+
+            ev.SomethingHappen += One.Handler;
+            ev.SomethingHappen += two.Handler;
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                ev.DoSmth(rand.Next(-10, 25));
+            }
+
+            ev.SomethingHappen -= One.Handler;
+            ev.SomethingHappen += () => Console.WriteLine("Anonymous handler");
+            ev.SomethingHappen += delegate
+            {
+                Console.WriteLine("delegate handler");
+            };
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                ev.DoSmth(rand.Next(-10, 25));
+            }
+        }
+    }
+}
