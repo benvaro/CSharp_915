3fb135482bccaa562a02858c3a0bc2b4eef5517c
benvaro <benvaro91@gmail.com>
2020-09-30 19:45:03 +0300

    EventHandler

---
+++ ButtonsDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ButtonsDemo
+{
+    class Program
+    {
+        class Button
+        {
+            public event EventHandler Click;
+            public void OnClick()
+            {
+                Click?.Invoke(null, null);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Button btn = new Button();
+            btn.Click += Btn_Click;
+            btn.OnClick();
+
+        }
+        private static void Btn_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button was clicked");
+        }
+    }
+}
